eff81fd7d2035684c9b3e56e4ce0e6d9ef559cf7
chukusuccess <successchuku5@gmail.com>
2022-10-17 11:08:43 +0100

    updates

--- pages/hyprep-projects/livelihoodProgramme.js
+++ pages/hyprep-projects/livelihoodProgramme.js
@@ -2,6 +2,11 @@ import React, { Fragment } from "react";
 import { CardOne } from "../../components/Card-one";
 import { CardTwo } from "../../components/Card-two";
 import MainLayout from "../../layout/MainLayout";
+const birds =
+  "https://hyprep.gov.ng/wp-content/uploads/2021/05/live1-768x1024.jpg";
+const meeting =
+  "https://hyprep.gov.ng/wp-content/uploads/2021/05/YOUTHS-300x200.jpg";
+const gifts = "https://hyprep.gov.ng/wp-content/uploads/2021/04/live3.png";
 
 const LivelihoodProgramme = () => {
   return (
@@ -27,12 +32,44 @@ const LivelihoodProgramme = () => {
           <section className="sm:py-10 sm:px-[140px] py-10 px-[30px] w-full bg-white flex flex-col items-center justify-center">
             <div className="sm:h-[663px] h-[331px] w-full flex flex-row items-center justify-center sm:gap-[35px] gap-[17px]">
               {/* this div below is for the mobile image */}
-              <div className="sm:hidden h-full sm:w-[479px] w-[100%] rounded-2xl bg-noContent"></div>
+              <div
+                className="sm:hidden h-full sm:w-[479px] w-[100%] rounded-2xl bg-noContent"
+                style={{
+                  background: `url(${meeting})`,
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              ></div>
               {/* the div below is for desktop images */}
-              <div className="hidden sm:flex h-full sm:w-[479px] w-[238px] rounded-2xl bg-noContent"></div>
+              <div
+                className="hidden sm:flex h-full sm:w-[479px] w-[238px] rounded-2xl bg-noContent"
+                style={{
+                  background: `url(${birds})`,
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              ></div>
               <div className="hidden h-full sm:w-[646px] w-[323px] sm:flex flex-col sm:gap-10 gap-5">
-                <div className="h-full w-full rounded-2xl bg-noContent"></div>
-                <div className="h-full w-full rounded-2xl bg-noContent"></div>
+                <div
+                  className="h-full w-full rounded-2xl bg-noContent"
+                  style={{
+                    background: `url(${meeting})`,
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+                <div
+                  className="h-full w-full rounded-2xl bg-noContent"
+                  style={{
+                    background: `url(${gifts})`,
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
               </div>
             </div>
           </section>
@@ -42,54 +79,40 @@ const LivelihoodProgramme = () => {
                 LIVELIHOOD
               </h3>
               <h2 className="sm:text-3xl text-xl font-merriweather font-bold pb-6">
-                Lorem ipsum dolor sit amet,
-                <br /> consectetur adipiscing elit.
+                Hon. Minister of Environment launches <br /> Livelihood Needs
+                Assessment Forms
               </h2>
               <p className="text-left">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
-                vitae proin interdum vivamus venenatis eu. Netus nisl magna
-                volutpat volutpat est, arcu ultricies eu. Tempus sagittis,
-                varius vel volutpat integer consequat. Ultrices elit feugiat
-                amet porttitor sed enim nisl eget lorem. Tincidunt lorem ante
-                sed hendrerit. Sagittis leo turpis porttitor facilisis euismod.
-                Varius maecenas purus magna nisi, risus, sem mauris tempor.
-                Pellentesque ut tempus morbi urna vel nisi, non. Tellus vitae
-                aliquet blandit eget ut aenean eu cursus. Vestibulum vel
-                scelerisque pretium, est elit, dolor libero enim, elit.
-                Dignissim etiam nunc, egestas nibh orci. Faucibus id duis libero
-                diam vestibulum tortor, iaculis. Aenean massa feugiat turpis non
-                in nullam. Sed cursus gravida ac vel mi nibh feugiat. Sit ac
-                diam velit pharetra. Vel turpis posuere velit, ac. Turpis
-                volutpat etiam tincidunt malesuada risus dignissim est. Tortor
-                leo nunc metus tellus mollis nisi, tortor imperdiet eu.
-                Sollicitudin lorem elit eget pharetra sit at neque tortor
-                libero. Venenatis, consectetur rhoncus praesent amet quam.
-                Ultrices vitae risus, ullamcorper dolor tellus magna massa
-                varius rhoncus. Vestibulum gravida tempus odio purus tortor
-                scelerisque adipiscing non. Venenatis, massa id tempus,
-                fermentum, ut. Aliquam nulla convallis tellus velit rhoncus
-                morbi massa. Nec interdum maecenas lectus fermentum semper a
-                nunc bibendum. Aenean neque egestas et egestas sit tempus. Massa
-                nisi, rhoncus in non commodo consectetur etiam nulla maecenas.
-                Vitae ornare massa gravida et sit sit arcu duis. Luctus sed amet
-                sit sed nisl. Odio nam amet, sagittis, sed sapien, lorem sit.
-                Porta ac varius augue gravida neque. Vestibulum, scelerisque
-                nunc, hac massa, porttitor aliquam pellentesque amet. Ornare
-                nulla fermentum, viverra sit erat accumsan arcu. Viverra cursus
-                velit, euismod justo, eu nulla. Cras nunc duis cursus non vitae
-                mattis dictumst porttitor nulla. Dolor commodo sed venenatis sit
-                neque nisl odio vestibulum amet. Cras lobortis purus egestas
-                tortor purus laoreet sed scelerisque. Volutpat neque erat
-                vestibulum lectus tincidunt est justo. Libero at in morbi tellus
-                convallis. Tincidunt non at ultricies luctus pharetra. Cras
-                malesuada enim, risus eu. Tristique ut purus sodales amet.
-                Sodales fermentum ultricies dictumst nunc aliquam imperdiet
-                venenatis tempor, lectus. Ac pretium, praesent suspendisse sit
-                orci nullam tortor nulla. Quis arcu est facilisis ut sed felis.
-                Ac dictum auctor tristique leo sapien commodo. Quis amet
-                scelerisque leo, sed urna ullamcorper velit cursus. In eget
-                porttitor donec vitae, ullamcorper pellentesque morbi feugiat
-                faucibus. Sit viverra vitae tellus nunc commodo, lectus.
+                On 22nd March 2021, at the occasion of the Flag-off of water
+                projects in Bori, Khana LGA, the Hon. Minister of Environment
+                launched the Livelihood Needs Assessment forms for the training
+                of youths and women under the Livelihood Programme of the
+                Hydrocarbon Pollution Remediation Project (HYPREP) towards the
+                full implementation of the recommendation of UNEP report on the
+                restoration of livelihoods of the Ogoni people. <br />
+                While launching the forms, the Minister noted that the measure
+                is in furtherance of the renewed efforts by HYPREP towards
+                restoration of livelihoods in Ogoniland. He explained that the
+                Needs Assessment will enable the project identify skill areas
+                for training of 5000 youths and women in Ogoniland and stated
+                that this will complement the effort of government in job
+                creation and maintained that the clean-up project is
+                comprehensive as other aspects are also being implemented
+                alongside remediation. He recalled successes recorded thus far
+                in the livelihood programme to include the training of 400 women
+                in agro-allied skills and the partnership with Stakeholder
+                Democracy Network (SDN) for the training of 15 Ogoni youths in
+                the fabrication of cassava processing machines.
+                <br />
+                <br />
+                <b>Distribution of Support Items to Cooperatives</b>
+                <br />
+                Also, within the month of March 2021, the Livelihood Unit
+                continued the distribution of support items to the twenty (20)
+                Cooperatives set up for the four hundred (400) Ogoni women
+                training programme. The distribution of the support items was
+                carried out in conjunction with our implementing partner, the
+                United Nations Institute for Training and Research (UNITAR).
               </p>
             </div>
           </section>
@@ -124,7 +147,7 @@ const LivelihoodProgramme = () => {
           <section className="sm:py-10 sm:px-[140px] py-10 px-[30px] w-full bg-white flex flex-col items-center">
             <div className="text-center mb-10">
               <h3 className="uppercase text-sm mb-5 text-textPrimary font-semibold">
-                Electricity reports
+                Livelihood reports
               </h3>
               <h2 className="sm:text-3xl text-xl font-merriweather font-bold pb-6">
                 Lorem ipsum dolor sit amet, <br />
